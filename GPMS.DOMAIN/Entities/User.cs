e97c7f769ec91c458023297046e972b39c855f83
Lawlez57 <trungnthe173045@fpt.edu.vn>
2026-02-26 11:30:46 +0700

    Add HATEOS form

--- GPMS.DOMAIN/Entities/User.cs
+++ GPMS.DOMAIN/Entities/User.cs
@@ -9,18 +9,11 @@ namespace GPMS.DOMAIN.Entities
     public class User
     {
        public int Id { get; set; }
-
        public string Username { get; set; }
-       
        public string PasswordHash { get; set; }
-
        public string FullName { get; set; }
-        
        public string PhoneNumber { get; set; }
-
        public string AvartarUrl { get; set; }
-
        public string Email { get; set; }   
-
     }
 }
