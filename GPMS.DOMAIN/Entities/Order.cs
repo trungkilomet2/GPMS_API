4178eeb2c06241e86c77c08fcccb3c4ae9a990ac
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-03-04 09:57:02 +0700

    Create Order.cs

---
+++ GPMS.DOMAIN/Entities/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMS.DOMAIN.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public string? Image { get; set; }
+       
+        public string OrderName { get; set; }
+     
+        public string Type { get; set; }
+
+        public string? Size { get; set; }
+
+        public string Color { get; set; } = null!;
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public int Quantity { get; set; }
+        
+        public decimal? Cpu { get; set; }
+       
+        public string? Note { get; set; }
+    }
+}
