0130337379b4a586f43e3a110912c9506714207d
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-03-04 11:57:34 +0700

    Update SqlServerToEntityProfile.cs

--- GPMS.INFRASTRUCTURE/Mappers/SqlServerToEntityProfile.cs
+++ GPMS.INFRASTRUCTURE/Mappers/SqlServerToEntityProfile.cs
@@ -12,9 +12,10 @@ namespace GPMS.INFRASTRUCTURE.Mappers
         public SqlServerToEntityProfile()
         {
             CreateMap<GPMS.INFRASTRUCTURE.DataContext.USER, GPMS.DOMAIN.Entities.User>()
-                .ForMember(dest => dest.Id,opt => opt.MapFrom(src => src.USER_ID))
-                .ForMember(dest => dest.PhoneNumber,opt => opt.MapFrom(src => src.PHONE_NUMBER));
-                
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.USER_ID))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PHONE_NUMBER)).ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FULLNAME)).ReverseMap();
+
+
             CreateMap<GPMS.INFRASTRUCTURE.DataContext.ROLE, GPMS.DOMAIN.Entities.Role>();
 
             CreateMap<GPMS.INFRASTRUCTURE.DataContext.UO_COMMENT, GPMS.DOMAIN.Entities.Comment>()
