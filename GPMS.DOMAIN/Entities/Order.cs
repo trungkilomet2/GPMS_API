d7489d0697d69c5766ed5a25182684213f32ac04
jngon9 <nghianhhe182026@fpt.edu.vn>
2026-03-06 10:22:46 +0700

    Update Order.cs

--- GPMS.DOMAIN/Entities/Order.cs
+++ GPMS.DOMAIN/Entities/Order.cs
@@ -10,26 +10,17 @@ namespace GPMS.DOMAIN.Entities
 {
     public class Order
     {
-        public int OrderId { get; set; }
-
+        public int Id { get; set; }
+        public int UserId { get; set; }
         public string? Image { get; set; }
-       
         public string OrderName { get; set; }
-     
         public string Type { get; set; }
-
         public string? Size { get; set; }
-
-        public string Color { get; set; } = null!;
-
+        public string Color { get; set; }
         public DateOnly StartDate { get; set; }
-
         public DateOnly EndDate { get; set; }
-
         public int Quantity { get; set; }
-        
         public decimal? Cpu { get; set; }
-       
         public string? Note { get; set; }
         public string Status { get; set; }
         public IEnumerable<OTemplate> Templates { get; set; } = new List<OTemplate>();
