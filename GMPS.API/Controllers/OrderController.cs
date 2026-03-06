a8f98fbdc9917112a07ada670a54f310da905c13
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-03-06 12:08:51 +0700

    Update OrderController.cs

--- GMPS.API/Controllers/OrderController.cs
+++ GMPS.API/Controllers/OrderController.cs
@@ -170,7 +170,6 @@ namespace GMPS.API.Controllers
                     Status = StatusCodes.Status500InternalServerError,
                     Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1"
                 };
-                Console.WriteLine(ex.Message + "dsadasdas");
                 return StatusCode(StatusCodes.Status500InternalServerError,exceptionDetails.Detail);
             }
 
