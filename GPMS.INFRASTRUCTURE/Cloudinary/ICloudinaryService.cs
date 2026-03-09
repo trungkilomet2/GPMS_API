61e52b8ecdb02059f4b3e891241dec617cc63a38
Lawlez57 <trungnthe173045@fpt.edu.vn>
2026-03-09 17:42:31 +0700

    Add Cloudinary

---
+++ GPMS.INFRASTRUCTURE/Cloudinary/ICloudinaryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMS.INFRASTRUCTURE.Cloudinary
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImageAsync(IFormFile file);
+    }
+}
