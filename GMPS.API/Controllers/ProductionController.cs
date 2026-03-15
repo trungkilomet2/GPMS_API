dbdd30021078c5f67605e053c42c044b238d4180
Lawlez57 <trungnthe173045@fpt.edu.vn>
2026-03-15 18:45:14 +0700

    Chang Url API

--- GMPS.API/Controllers/ProductionController.cs
+++ GMPS.API/Controllers/ProductionController.cs
@@ -19,7 +19,7 @@ namespace GMPS.API.Controllers
             _productionService = productionService;
         }
 
-        [HttpPost("create-production")]
+        [HttpPost("production/create")]
         public async Task<ActionResult<MessageResponseDTO<Production>>> CreateProduction([FromBody] CreateProductionDTO dto)
         {
             try
@@ -58,23 +58,23 @@ namespace GMPS.API.Controllers
             }
         }
 
-        [HttpGet("get-list-production")]
+        [HttpGet("production/list")]
         public async Task<ActionResult<IEnumerable<Production>>> GetList()
             => Ok(await _productionService.GetProductionList());
 
-        [HttpGet("production-detail/{id:int}")]
+        [HttpGet("production/detail/{id:int}")]
         public async Task<ActionResult<Production>> GetDetail(int id)
             => Ok(await _productionService.GetProductionDetail(id));
 
-        [HttpPatch("{id:int}/revision-request")]
+        [HttpPatch("revision-request/{id:int}")]
         public async Task<ActionResult<Production>> RequestRevision(int id)
             => Ok(await _productionService.RequestProductionRevision(id));
 
-        [HttpPatch("{id:int}/deny")]
+        [HttpPatch("production/deny/{id:int}")]
         public async Task<ActionResult<Production>> Deny(int id, [FromBody] RejectProductionDTO dto)
             => Ok(await _productionService.DenyProduction(id, dto.UserId, dto.Reason));
 
-        [HttpPut("{id:int}")]
+        [HttpPut("production/update/{id:int}")]
         public async Task<ActionResult<Production>> Update(int id, [FromBody] UpdateProductionDTO dto)
             => Ok(await _productionService.UpdateProduction(id, new Production
             {
@@ -83,15 +83,15 @@ namespace GMPS.API.Controllers
               //  StatusId = dto.StatusId
             }));
 
-        [HttpGet("plans/pending")]
+        [HttpGet("production-plans/pending")]
         public async Task<ActionResult<IEnumerable<Production>>> GetPendingPlans()
             => Ok(await _productionService.GetPendingProductionPlans());
 
-        [HttpGet("plans")]
+        [HttpGet("production-plans")]
         public async Task<ActionResult<IEnumerable<Production>>> GetPlanList()
             => Ok(await _productionService.GetProductionPlanList());
 
-        [HttpPost("{id:int}/plans/config")]
+        [HttpPost("production-plans/config/{id:int}")]
         public async Task<ActionResult<Production>> ConfigPlan(int id, [FromBody] List<ProductionPartDTO> dto)
             => Ok(await _productionService.ConfigProductionPlan(id, dto.Select(x => new ProductionPart
             {
@@ -103,11 +103,11 @@ namespace GMPS.API.Controllers
                 StatusId = x.StatusId
             })));
 
-        [HttpGet("{id:int}/plans/detail")]
+        [HttpGet("production-plans/detail/{id:int}")]
         public async Task<ActionResult<Production>> GetPlanDetail(int id)
             => Ok(await _productionService.GetProductionPlanDetail(id));
 
-        [HttpPatch("{id:int}/plans/deny")]
+        [HttpPatch("production-plans/deny/{id:int}")]
         public async Task<ActionResult<Production>> DenyPlan(int id, [FromBody] RejectProductionDTO dto)
             => Ok(await _productionService.DenyProductionPlan(id, dto.UserId, dto.Reason));
     }
