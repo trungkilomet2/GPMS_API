e47fbb6e32277688205865f8bd2f57820db4cb5a
Lawlez57 <trungnthe173045@fpt.edu.vn>
2026-03-16 02:16:27 +0700

    Add DTO

---
+++ GMPS.API/DTOs/ListProductionDTO.cs
@@ -0,0 +1,14 @@
+﻿using GPMS.DOMAIN.Entities;
+
+namespace GMPS.API.DTOs
+{
+    public class ListProductionDTO
+    {
+        public int Id { get; set; }
+        public int PmId { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public int StatusId { get; set; }
+        public IEnumerable<Order> Order { get; set; } = new List<Order>();
+    }
+}
