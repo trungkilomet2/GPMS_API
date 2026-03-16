e5577480eb5fc6c6ff64079a8319219808c17e1b
Lawlez57 <trungnthe173045@fpt.edu.vn>
2026-03-16 11:50:16 +0700

    Remove FK

--- GPMS.INFRASTRUCTURE/Mappers/SqlServerToEntityProfile.cs
+++ GPMS.INFRASTRUCTURE/Mappers/SqlServerToEntityProfile.cs
@@ -134,7 +134,6 @@ namespace GPMS.INFRASTRUCTURE.Mappers
                 .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.P_START_DATE))
                 .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => src.P_END_DATE))
                 .ForMember(dest => dest.StatusId, opt => opt.MapFrom(src => src.PS_ID))
-              //  .ForMember(dest => dest.Order, opt => opt.MapFrom(src => src.ORDER))
                 .ReverseMap()
                 .ForMember(dest => dest.PRODUCTION_ID, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.PM_ID, opt => opt.MapFrom(src => src.PmId))
@@ -142,7 +141,6 @@ namespace GPMS.INFRASTRUCTURE.Mappers
                 .ForMember(dest => dest.P_START_DATE, opt => opt.MapFrom(src => src.StartDate))
                 .ForMember(dest => dest.P_END_DATE, opt => opt.MapFrom(src => src.EndDate))
                 .ForMember(dest => dest.PS_ID, opt => opt.MapFrom(src => src.StatusId))
-            //    .ForMember(dest => dest.ORDER, opt => opt.MapFrom(src => src.Order))
                 ;
 
 
