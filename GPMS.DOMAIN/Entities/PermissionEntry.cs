4dc728aa5920c472163b5098c013c04230a520a1
jngon9 <nghianhhe182026@fpt.edu.vn>
2026-03-19 20:58:45 +0700

    // methodpermissionforfix

--- GPMS.DOMAIN/Entities/PermissionEntry.cs
+++ GPMS.DOMAIN/Entities/PermissionEntry.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace GPMS.DOMAIN.Entities
-{
-    public class PermissionEntry
-    {
-        public string Controller { get; set; }
-        public string Action { get; set; }
-        public string RoleIds { get; set; }
+//namespace GPMS.DOMAIN.Entities
+//{
+//    public class PermissionEntry
+//    {
+//        public string Controller { get; set; }
+//        public string Action { get; set; }
+//        public string RoleIds { get; set; }
 
-        public PermissionEntry(string controller, string action, string roleIds)
-        {
-            Controller = controller;
-            Action = action;
-            RoleIds = roleIds;
-        }
-    }
-}
+//        public PermissionEntry(string controller, string action, string roleIds)
+//        {
+//            Controller = controller;
+//            Action = action;
+//            RoleIds = roleIds;
+//        }
+//    }
+//}
