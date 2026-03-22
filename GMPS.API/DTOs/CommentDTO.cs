5b6eb2292ee19c8392d7185250bb7b182a93c1c8
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-03-22 09:46:04 +0700

    fix

--- GMPS.API/DTOs/CommentDTO.cs
+++ GMPS.API/DTOs/CommentDTO.cs
@@ -6,7 +6,6 @@ namespace GMPS.API.DTOs
     public class CommentDTO
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
         public int ToOrderId { get; set; }
         public string Content { get; set; }
         public DateTime SendDateTime { get; set; }
