c91d2939504ef50c63e79258fc5e5d6ce59b5cb7
Lawlez57 <trungnthe173045@fpt.edu.vn>
2026-03-22 16:55:24 +0700

    Add IRepositories

---
+++ GPMS.APPLICATION/Repositories/ITemplateRepositories.cs
@@ -0,0 +1,11 @@
+﻿using GPMS.DOMAIN.Entities;
+using GPMS.DOMAIN.Entities.GPMS.DOMAIN.Entities;
+
+namespace GPMS.APPLICATION.Repositories
+{
+    public interface ITemplateRepositories
+    {
+        Task<TemplateDefinition> Create(TemplateDefinition entity);
+        Task<IEnumerable<TemplateDefinition>> GetAll();
+    }
+}
