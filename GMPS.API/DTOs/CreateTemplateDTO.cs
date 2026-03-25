a311ac67af39c64a32a9477879d16b6802e891ff
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-03-25 22:25:16 +0700

    fix validate DTO
    sửa lại validation cho data cho trùng với db

--- GMPS.API/DTOs/CreateTemplateDTO.cs
+++ GMPS.API/DTOs/CreateTemplateDTO.cs
@@ -9,13 +9,13 @@ namespace GMPS.API.DTOs
         [StringLength(100, ErrorMessage = "TemplateName cannot exceed 100 characters")]
         public string TemplateName { get; set; }
 
-        [StringLength(5, ErrorMessage = "Type cannot exceed 5 characters")]
+        [StringLength(50, ErrorMessage = "Type cannot exceed 50 characters")]
         public string? Type { get; set; }
 
         [StringLength(255, ErrorMessage = "File path cannot exceed 255 characters")]
         public string? File { get; set; }
 
-        [MaxLength(500, ErrorMessage = "Note cannot exceed 500 characters")]
+        [MaxLength(100, ErrorMessage = "Note cannot exceed 100 characters")]
         public string? Note { get; set; }
     }
 }
