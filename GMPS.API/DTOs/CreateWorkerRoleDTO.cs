a0fc46fb8e971ae319d784368822be7ccf9a8ca7
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-03-29 21:53:58 +0700

    Update CreateWorkerRoleDTO.cs

--- GMPS.API/DTOs/CreateWorkerRoleDTO.cs
+++ GMPS.API/DTOs/CreateWorkerRoleDTO.cs
@@ -5,6 +5,7 @@ namespace GMPS.API.DTOs
     public class CreateWorkerRoleDTO
     {
         [Required(ErrorMessage ="Role Name is required")]
+        [StringLength(50, ErrorMessage = "tên kỹ năng không được vượt quá 50")]
         public string Name { get; set; }
     }
 }
