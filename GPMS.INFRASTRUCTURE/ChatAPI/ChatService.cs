66c5d9b241bc69488764c6dc19a0525075de5143
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-03-30 19:30:19 +0700

    Update ChatService.cs

--- GPMS.INFRASTRUCTURE/ChatAPI/ChatService.cs
+++ GPMS.INFRASTRUCTURE/ChatAPI/ChatService.cs
@@ -1,6 +1,7 @@
 using GPMS.APPLICATION.DTOs;
 using GPMS.APPLICATION.Repositories;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Text;
 using System.Text.Json;
 
