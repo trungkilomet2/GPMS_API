907f417bb5314af44060bfb45536b0542ea55144
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-04-01 12:13:35 +0700

    fix bug

--- GMPS.API/DTOs/CreateOrderDTO.cs
+++ GMPS.API/DTOs/CreateOrderDTO.cs
@@ -5,45 +5,46 @@ namespace GMPS.API.DTOs
 {
     public class CreateOrderDTO
     {
-        [Required(ErrorMessage = "UserId is required")]
-        [Range(1, int.MaxValue, ErrorMessage = "UserId must be greater than 0")]
+        [Required(ErrorMessage = "Phải có Id người dùng")]
+        [Range(1, int.MaxValue, ErrorMessage = "Id người dùng phải > 0")]
         public int UserId { get; set; }
 
-        [Url(ErrorMessage = "Image must be a valid URL")]
-        [StringLength(255, ErrorMessage = "Image URL is too long")]
+        [Url(ErrorMessage = "Link ảnh không hợp lệ")]
+        [StringLength(255, ErrorMessage = "Link ảnh quá dài")]
         public string? Image { get; set; }
 
-        [Required(ErrorMessage = "Order name is required")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Order name must be between 3 and 100 characters")]
+        [Required(ErrorMessage = "Yêu cầu nhập tên")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Tên phải từ 3 đến 100 ký tự")]
         public string OrderName { get; set; }
 
-        [Required(ErrorMessage = "Type is required")]
-        [StringLength(50, ErrorMessage = "Type cannot exceed 50 characters")]
+        [Required(ErrorMessage = "Yêu cầu nhập kiểu")]
+        [StringLength(50, ErrorMessage = "Kiểu không được vượt quá 5 ký tự")]
         public string Type { get; set; }
 
-        [StringLength(5, ErrorMessage = "Size cannot exceed 5 characters")]
+        [StringLength(5, ErrorMessage = "Kích thước không được vượt quá 5 ký tự")]
         [RegularExpression("^(XS|S|M|L|XL|XXL|XXXL)$",
     ErrorMessage = "Size must be XS, S, M, L, XL, XXL, or XXXL")]
         public string? Size { get; set; }
 
-        [Required(ErrorMessage = "Color is required")]
-        [StringLength(30, ErrorMessage = "Color cannot exceed 30 characters")]
+        [Required(ErrorMessage = "Yêu cầu chọn màu")]
+        [StringLength(30, ErrorMessage = "Màu không được vượt quá 30 ký tự")]
         public string Color { get; set; }
 
-        [Required(ErrorMessage = "StartDate is required")]
+        [Required(ErrorMessage = "Phải có ngày bắt đầu")]
         public DateOnly StartDate { get; set; }
 
-        [Required(ErrorMessage = "EndDate is required")]
+        [Required(ErrorMessage = "Phải có ngày kết thức")]
         public DateOnly EndDate { get; set; }
 
-        [Required(ErrorMessage = "Quantity is required")]
-        [Range(1, 1000, ErrorMessage = "Quantity must be greater than 0")]
+        [Required(ErrorMessage = "Yêu cầu nhập số lượng")]
+        [Range(1, 9999, ErrorMessage = "Số lượng phải từ 1 và không được vượt quá 9999")]
         public int Quantity { get; set; }
 
-        [Range(10, double.MaxValue, ErrorMessage = "Cpu must be greater than or equal to 0")]
+        [Required(ErrorMessage = "Yêu cầu nhập giá mỗi sản phẩm")]
+        [Range(1000, 10000000, ErrorMessage = "Giá mỗi sản phẩm phải từ 1000 và không được vượt quá 10000000")]
         public decimal? Cpu { get; set; }
 
-        [StringLength(200, ErrorMessage = "Note cannot exceed 200 characters")]
+        [StringLength(200, ErrorMessage = "Ghi chú không được vượt quá 200 ký tự")]
         public string? Note { get; set; }
         public DateTime? CreateTime { get; set; }
         public List<CreateMaterialDTO>? Materials { get; set; }
