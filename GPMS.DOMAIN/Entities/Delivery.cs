9880ec6b0418b1579b3a5f699bc415464e294d06
Lawlez57 <trungnthe173045@fpt.edu.vn>
2026-04-13 00:52:52 +0700

    Fix delivery

--- GPMS.DOMAIN/Entities/Delivery.cs
+++ GPMS.DOMAIN/Entities/Delivery.cs
@@ -9,17 +9,10 @@ namespace GPMS.DOMAIN.Entities
     public class Delivery
     {
         public int Id { get; set; }
-        
         public int OrderSizeId { get; set; }
-
         public int DeliverQuantity { get; set; }
-
-        public DateTime DeliveredAt { get; set; }
-
-        public DateTime ReceivedDate { get; set; }
-        public int? DeliverStatusId { get; set; }
-
-
-
+        public DateTime? DeliveredAt { get; set; }
+        public DateTime? ReceivedDate { get; set; }
+        public int DeliverStatusId { get; set; }
     }
 }
