04bbdf23ba50c17216f10d8b322f8dca1dc6b599
tranlongvu2004 <vutlhe181634@fpt.edu.vn>
2026-04-24 20:03:14 +0700

    fix chat box

---
+++ GPMS.DOMAIN/Constants/Chat_Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPMS.DOMAIN.Constants
+{
+    public class Chat_Constants
+    {
+        public const string ModelName = "google/gemini-2.5-flash";
+        public const string Uri = "chat/completions";
+        public const string MediaType = "application/json";
+    }
+}
